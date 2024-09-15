dac4fac8fb7e8b8bd0a90f2ebd80ed2500b146a3
jdw5 <joshuadwallace1@gmail.com>
2024-09-15 13:33:44 +1000

    Configured project

---
+++ src/Evaluate.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Conquest\Evaluate;
+
+class Evaluate 
+{
+    /**
+     * @var array
+     */
+    protected static $evaluations = [];
+
+    /**
+     * @var int
+     */
+    protected static $metrics = 0;
+
+    /**
+     * @var int
+     */
+    protected static $times = 1;
+
+    /**
+     * @var boolean
+     */
+    protected static $peak = true;
+
+    /**
+     * @var string|null
+     */
+    protected static $name = null;
+
+    // public static function measure(mixed $evaluations)
+    // public static function measure
+
+
+}
--- src/EvaluateServiceProvider.php
+++ src/EvaluateServiceProvider.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Conquest\Evaluate;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
+use Conquest\Evaluate\Commands\EvaluateCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class EvaluateServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
@@ -16,10 +16,10 @@ class SkeletonServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('skeleton')
+            ->name('evaluate')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_skeleton_table')
-            ->hasCommand(SkeletonCommand::class);
+            ->hasMigration('create_evaluate_table')
+            ->hasCommand(EvaluateCommand::class);
     }
 }
